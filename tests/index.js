56aff3dc8638d9a460228bed69eb3a22d5980ca5
David Mohr <david.mohr@ardexa.com>
2021-03-12 08:58:04 +1100

    chore: lint tests

--- tests/index.js
+++ tests/index.js
@@ -2,85 +2,81 @@ import chai from 'chai';
 import dirtyChai from 'dirty-chai';
 import Sudoer from '../src/index';
 
-let {expect} = chai,
-    {platform} = process,
-    options = {
-        name: 'Electron.Sudo',
-        icns: '/Applications/Automator.app/Contents/Resources/AutomatorApplet.icns'
-    };
+let { expect } = chai;
+let { platform } = process;
 
-let sudoer = new Sudoer(options);
+let sudoer = new Sudoer();
 chai.use(dirtyChai);
 
 describe(`electron-sudo :: ${platform}`, function () {
 
-    this.timeout(100000);
-    this.slow(100000);
+  this.timeout(100000);
+  this.slow(100000);
 
-    if (platform === 'darwin') {
-        describe('[exec] with ENV vars', async function () {
-            it('should available environment variables', async function () {
-                let result = await sudoer.exec('echo $PARAM', {env: {PARAM: 'VALUE'}});
-                expect(result.stdout.trim()).to.be.equals('VALUE');
-            });
+  if (platform === 'darwin') {
+    describe('[exec] with ENV vars', async function () {
+      it('should available environment variables', async function () {
+        let result = await sudoer.exec('echo $PARAM', {env: {PARAM: 'VALUE'}});
+        expect(result.stdout.trim()).to.be.equals('VALUE');
+      });
+    });
+    describe('[spawn] with ENV vars', async function () {
+      it('should available environment variables', async function () {
+        let cp = await sudoer.spawn('echo', ['$PARAM'], {env: {PARAM: 'VALUE'}});
+        return new Promise(resolve => {
+          cp.on('close', () => {
+            expect(cp.output.stdout.toString().trim()).to.be.equals('VALUE');
+            expect(cp.pid).to.be.a('number');
+            resolve();
+          });
         });
-        describe('[spawn] with ENV vars', async function () {
-            it('should available environment variables', async function () {
-                let cp = await sudoer.spawn('echo', ['$PARAM'], {env: {PARAM: 'VALUE'}});
-                return new Promise(resolve => {
-                    cp.on('close', () => {
-                        expect(cp.output.stdout.toString().trim()).to.be.equals('VALUE');
-                        expect(cp.pid).to.be.a('number');
-                        resolve();
-                    });
-                });
-            });
-        });
-    }
+      });
+    });
+  }
 
-    if (platform === 'linux') {
-        describe('[pkexec: exec] with ENV vars', async function () {
-            it('should available environment variables', async function () {
-                sudoer.binary = '/usr/bin/pkexec';
-                // sudoer.exec('echo $PARAM', {env: {PARAM: 'VALUE'}});
-                // await sudoer.exec('echo $PARAM', {env: {PARAM: 'VALUE'}});
-                let result = await sudoer.exec('echo $PARAM', {env: {PARAM: 'VALUE'}});
-                expect(result.stdout.trim()).to.be.equals('VALUE');
-            });
+  if (platform === 'linux') {
+    describe('[pkexec: exec] with ENV vars', async function () {
+      it('should available environment variables', async function () {
+        sudoer.binary = '/usr/bin/pkexec';
+        // sudoer.exec('echo $PARAM', {env: {PARAM: 'VALUE'}});
+        // await sudoer.exec('echo $PARAM', {env: {PARAM: 'VALUE'}});
+        let result = await sudoer.exec('echo $PARAM', {env: {PARAM: 'VALUE'}});
+        expect(result.stdout.trim()).to.be.equals('VALUE');
+      });
+    });
+    describe('[pkexec: spawn] with ENV vars', async function () {
+      it('should available environment variables', async function () {
+        sudoer.binary = '/usr/bin/pkexec';
+        let cp = await sudoer.spawn('echo', ['$PARAM'], {env: {PARAM: 'VALUE'}});
+        return new Promise(resolve => {
+          cp.on('close', () => {
+            expect(cp.output.stdout.toString().trim()).to.be.equals('VALUE');
+            expect(cp.pid).to.be.a('number');
+            resolve();
+          });
         });
-        describe('[pkexec: spawn] with ENV vars', async function () {
-            it('should available environment variables', async function () {
-                sudoer.binary = '/usr/bin/pkexec';
-                let cp = await sudoer.spawn('echo', ['$PARAM'], {env: {PARAM: 'VALUE'}});
-                return new Promise(resolve => {
-                    cp.on('close', () => {
-                        expect(cp.output.stdout.toString().trim()).to.be.equals('VALUE');
-                        expect(cp.pid).to.be.a('number');
-                        resolve();
-                    });
-                });
-            });
-        });
-    }
+      });
+    });
+  }
 
-    if (platform === 'win32') {
-        describe('[exec] with ENV vars', async function () {
-            it('should available environment variables', async function () {
-                let result = await sudoer.exec('echo %PARAM%', {env: {PARAM: 'VALUE'}});
-                expect(result.toString().trim()).to.be.equals('VALUE');
-            });
+  if (platform === 'win32') {
+    describe('[exec] with ENV vars', async function () {
+      it('should available environment variables', async function () {
+        let result = await sudoer.exec('echo %PARAM%', {env: {PARAM: 'VALUE'}});
+        expect(result.toString().trim()).to.be.equals('VALUE');
+      });
+    });
+    describe('[spawn] with ENV vars', async function () {
+      it('should available environment variables', async function () {
+        let cp = await sudoer.spawn('echo', ['%PARAM%'], {env: {PARAM: 'VALUE'}});
+        return new Promise(resolve => {
+          cp.on('close', () => {
+            expect(cp.output.stdout.toString().trim()).to.be.equals('VALUE');
+            expect(cp.pid).to.be.a('number');
+            resolve();
+          });
         });
-        describe('[spawn] with ENV vars', async function () {
-            it('should available environment variables', async function () {
-                let cp = await sudoer.spawn('echo', ['%PARAM%'], {env: {PARAM: 'VALUE'}});
-                return new Promise(resolve => {
-                    cp.on('close', () => {
-                        expect(cp.output.stdout.toString().trim()).to.be.equals('VALUE');
-                        expect(cp.pid).to.be.a('number');
-                        resolve();
-                    });
-                });
-            });
-        });
-    }
+      });
+    });
+  }
 });
